3e841b0809a9cbb811034d21dc391dd3d7faa749
Carlos Peralta <peralta.lima.carlos@gmail.com>
2020-11-17 13:11:44 -0400

    Error1

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+        title: 'IOT',
       theme: ThemeData(
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
