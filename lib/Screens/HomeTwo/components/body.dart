2f2ceac5cb86da62a707887854bb370688348259
Carlos Peralta <peralta.lima.carlos@gmail.com>
2020-11-22 09:02:00 -0400

    Segunda Parte Pantallas

--- lib/Screens/HomeTwo/components/body.dart
+++ lib/Screens/HomeTwo/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:security_iot_system/Screens/Details/details_screen.dart';
+import 'package:security_iot_system/components/reading_card_list.dart';
 
 import '../../../size_config.dart';
 import 'categories.dart';
@@ -17,8 +19,37 @@ class Body extends StatelessWidget {
             SizedBox(height: getProportionateScreenHeight(20)),
             HomeHeader(),
             SizedBox(height: getProportionateScreenWidth(10)),
-            DiscountBanner(),
             Categories(),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: <Widget>[
+                  Accionador(
+                    image: "assets/images/q12w.jpg",
+                    title: "Crushing & Influence",
+                    auth: "Gary Venchuk",
+                    rating: 4.9,
+                    pressDetails: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return DetailsScreen();
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                  Accionador(
+                    image: "assets/images/q12w.jpg",
+                    title: "Top Ten Business Hacks",
+                    auth: "Herman Joel",
+                    rating: 4.8,
+                  ),
+                  SizedBox(width: 30),
+                ],
+              ),
+            ),
             SpecialOffers(),
             SizedBox(height: getProportionateScreenWidth(30)),
             PopularProducts(),
---
+++ lib/components/reading_card_list.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:security_iot_system/components/two_side_rounded_button.dart';
+import 'package:security_iot_system/constants.dart';
+
+import 'book_rating.dart';
+
+class Accionador extends StatelessWidget {
+  final String image;
+  final String title;
+  final String auth;
+  final double rating;
+  final Function pressDetails;
+  final Function pressRead;
+
+  const Accionador({
+    Key key,
+    this.image,
+    this.title,
+    this.auth,
+    this.rating,
+    this.pressDetails,
+    this.pressRead,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 24, bottom: 40),
+      height: 245,
+      width: 202,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 221,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(29),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 33,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Image.asset(
+            image,
+            width: 150,
+          ),
+          Positioned(
+            top: 35,
+            right: 10,
+            child: Column(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.favorite_border,
+                  ),
+                  onPressed: () {},
+                ),
+                BookRating(score: rating),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 160,
+            child: Container(
+              height: 85,
+              width: 202,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(left: 24),
+                    child: RichText(
+                      maxLines: 2,
+                      text: TextSpan(
+                        style: TextStyle(color: kPrimaryColor),
+                        children: [
+                          TextSpan(
+                            text: "$title\n",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: auth,
+                            style: TextStyle(
+                              color: kPrimaryColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  Row(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: pressDetails,
+                        child: Container(
+                          width: 101,
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          alignment: Alignment.center,
+                          child: Text("Details"),
+                        ),
+                      ),
+                      Expanded(
+                        child: TwoSideRoundedButton(
+                          text: "Read",
+                          press: pressRead,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
